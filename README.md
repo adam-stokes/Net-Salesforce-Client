5ff69860133bee0331bd079af9d5fb444deb1337
Adam Stokes <adam.stokes@ubuntu.com>
2014-01-06 22:42:24 -0500

    initial
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::Salesforce::Client - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::Salesforce::Client;
+
+# DESCRIPTION
+
+Net::Salesforce::Client is
+
+# AUTHOR
+
+Adam Stokes <adamjs@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2014- Adam Stokes
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/Salesforce/Client.pm
@@ -0,0 +1,39 @@
+package Net::Salesforce::Client;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Salesforce::Client - Salesforce.com client utilities
+
+=head1 SYNOPSIS
+
+  use Net::Salesforce::Client;
+
+=head1 DESCRIPTION
+
+Net::Salesforce::Client is a perl interface to Salesforce.com JSON api.
+
+=head1 AUTHOR
+
+Adam Stokes E<lt>adamjs@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- Adam Stokes
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
